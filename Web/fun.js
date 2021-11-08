8d12ebed70cddd671c0649bb6795d360e09f0c10
Tessa <33944@Ma-web.nl>
2021-11-08 14:14:44 +0100

    Mystery page onderdelen.

---
+++ Web/fun.js
@@ -0,0 +1,21 @@
+function click1(){
+    document.body.style.backgroundColor = "#E68000"; 
+}
+
+function click2(){
+    document.body.style.color = "#0000ff"; 
+}
+
+function click3(){
+    document.body.style.backgroundColor = "Black";
+    document.body.style.color = "Grey";
+  
+}
+
+function gif(){
+    document.getElementById('Cat').style.visibility="visible";
+    document.body.style.backgroundColor = "Green";
+    document.body.style.color = "PINK";
+    document.getElementById('SoundCat').play(); 
+}
+
